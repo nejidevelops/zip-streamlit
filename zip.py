504810090f083be53c8d2b5989eb0031809a2d47
Newton <newtonombese1@gmail.com>
2024-02-20 06:29:42 +0300

    Successful setup of the application

--- zip.py
+++ zip.py
@@ -1,3 +1,21 @@
+import gzip
 import streamlit as st
 
 st.title("Compress Files")
+
+uploaded_file = st.file_uploader(
+    "File to Compress",
+    "txt",
+)
+
+if uploaded_file is not None:
+    data = uploaded_file.read()
+    compressed_data = gzip.compress(data)
+    st.download_button(
+        "Download Compressed File",
+        compressed_data,
+        file_name=uploaded_file.name + ".gz",
+    )
+    # with open(uploaded_file.name, "wb") as f:
+    #     f.write(uploaded_file.getbuffer())
+    # st.success("File Saved")
